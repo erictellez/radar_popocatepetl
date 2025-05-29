984b92947f27cfd4eaf47cadb728da66938e28ba
Eric Tellez <66031565+erictellez@users.noreply.github.com>
2025-05-29 17:19:16 -0600

    Add files via upload

---
+++ radar_connection_wTelegram.ps1
@@ -0,0 +1,40 @@
+﻿#This program is to test the connection
+#With help of Task Scheduler of Windows we can test the connection every some minutes
+#Eric Tellez, March 2023
+
+Write-Output "This program is testing the connection to Altzomoni"
+
+#To test the connection with a ping
+$uno= Test-Connection -ComputerName 192.168.10.50 -Quiet     #IP address of computer in Geophysics Institute
+$dos= Test-Connection -ComputerName 192.168.10.52 -Quiet     #IP address of radio transmitter in UNAM
+$tres= Test-Connection -ComputerName 192.168.10.56 -Quiet    #IP address of radio transmitter in Altzomoni
+$cuatro= Test-Connection -ComputerName 192.168.10.60 -Quiet  #IP address of computer in Altzomoni
+
+#Write-Output $uno
+#Write-Output $dos
+#Write-Output $tres
+#Write-Output $cuatro
+
+
+if ( ($uno -match "False") -or ($dos -match "False") -or ($tres -match "False") -or ($cuatro -match "False") ) 
+    { 
+    #If some part of the connection is broken, then a robotic voice warns about it
+    (New-Object -com SAPI.SpVoice).speak("Falló la conexión")
+    
+    #This piece of code is to send a message to Telegram when the connection fails
+    $Telegramtoken = "6156678877:AAEtwGzJSd3OqnXlScsYSfydQPMjgHDEXXY"
+    $Telegramchatid = "622176669"
+    $Message = "Falló la conexión"
+
+    [Net.ServicePointManager]::Expect100Continue = $true
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+    $irmParams = @{
+        Uri = "https://api.telegram.org/bot$($Telegramtoken)/sendMessage?chat_id=$($Telegramchatid)&text=$($Message)"
+    }
+
+    $Response = Invoke-RestMethod @irmParams
+    
+    #To make a beep instead of the robotic voice
+    #[console]::beep(1000,500)
+    }
